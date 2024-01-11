9645d6ff10c6ea9bed5dd54aff629ec705ece00a
balog-martin <balogmartin92@gmail.com>
2024-01-11 15:15:17 +0100

    Olvasasi idö hozzaadva

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function calcReadTime()
+{ 
+    let text = document.getElementById("idArticle").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const timeMin = Math.ceil(words / wpm);
+
+    return timeMin;
+}
+
+function updateReadTime()
+{
+    document.getElementById("idReadTimeValue").innerText = calcReadTime();
+}
+
+updateReadTime();
+calcReadTime();
