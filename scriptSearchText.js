497ab5c35c2095f104c7a2c459925fe8bd50397d
balog-martin <balogmartin92@gmail.com>
2024-01-11 21:02:25 +0100

    Masik megközelites: change callback

--- scriptSearchText.js
+++ scriptSearchText.js
@@ -2,6 +2,32 @@ function Search(e)
 {
     //Debug:
     console.log(e);
+    let searchInput = document.getElementById("#idSearchInput").innerText;
+    console.log(searchInput);
+
 }
 
-document.querySelector("#idSearchInput").addEventListener("keydown", (e) => Search(e))
+//document.querySelector("idSearchInput").addEventListener("change", (updateValue) => Search(updateValue))
+//ocument.querySelector("#idSearchInput").addEventListener("change", updateValue);
+
+// ----------------------
+
+// const input = document.querySelector("#idSearchInput");
+// // const log = document.getElementById("log");
+
+// input.addEventListener("change", updateValue);
+
+// function updateValue(e) {
+//   console.log(e);
+// }
+
+//###########################
+const input = document.querySelector("#idSearchInput");
+const siteOriginal = document.getElementById("idArticle").innerText;
+
+
+input.addEventListener("change", updateValue);
+
+function updateValue(e) {
+  console.log(e.target.value);
+}
