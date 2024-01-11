1e52842d4686faba639b0b70eab375535e927238
balog-martin <balogmartin92@gmail.com>
2024-01-11 21:20:47 +0100

    Egyet mar talal, többet meg nem

--- scriptSearchText.js
+++ scriptSearchText.js
@@ -4,30 +4,33 @@ function Search(e)
     console.log(e);
     let searchInput = document.getElementById("#idSearchInput").innerText;
     console.log(searchInput);
-
 }
 
 //document.querySelector("idSearchInput").addEventListener("change", (updateValue) => Search(updateValue))
 //ocument.querySelector("#idSearchInput").addEventListener("change", updateValue);
 
-// ----------------------
-
-// const input = document.querySelector("#idSearchInput");
-// // const log = document.getElementById("log");
-
-// input.addEventListener("change", updateValue);
-
-// function updateValue(e) {
-//   console.log(e);
-// }
-
 //###########################
 const input = document.querySelector("#idSearchInput");
-const siteOriginal = document.getElementById("idArticle").innerText;
+const siteOriginal = document.getElementById("idArticle").innerHTML;
 
 
 input.addEventListener("change", updateValue);
 
 function updateValue(e) {
-  console.log(e.target.value);
+    let txtToSearch = e.target.value; 
+    console.log(txtToSearch);
+
+    let txtHighlighted = "<span class=searchHighlight>" + txtToSearch + "</span>";
+    let siteHighlighted = siteOriginal.replace(txtToSearch, txtHighlighted);
+
+    console.log(txtHighlighted);
+    console.log(siteHighlighted);
+
+    document.getElementById("idArticle").innerHTML = siteHighlighted;
+}
+
+function highlightFoundText()
+{
+    let siteHighlighted = siteOriginal;
+    
 }
