92f646af37abb46115b6e2ca8bf034dfe3c23081
balog-martin <balogmartin92@gmail.com>
2024-01-11 15:18:54 +0100

    Komment s+/

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function calcReadTime()
 { 
     let text = document.getElementById("idArticle").innerText;
     const wpm = 225;
-    const words = text.trim().split(/\s+/).length;
+    const words = text.trim().split(/\s+/).length; // \s+: It's a regular expression that splits the string with \s matching single whitespace characters and \s+ matching one or more whitespace characters.
     const timeMin = Math.ceil(words / wpm);
 
     return timeMin;
