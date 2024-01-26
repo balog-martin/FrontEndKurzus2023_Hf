0231b25e640997cbff6695c9d66582e8b81c5736
balog-martin <balogmartin92@gmail.com>
2024-01-26 22:27:47 +0100

    Müködik az email-cim linkkent, de a formazas meg nem teljesen korrekt

--- scriptFetchApi.js
+++ scriptFetchApi.js
@@ -8,21 +8,34 @@ async function addUserInfoToArticle(user)
 {
     let divAuthor = document.querySelector("#idAuthor");
     let h2 = document.createElement('h2');
-    h2.innerHTML += "Szerzö";
+    h2.innerHTML += "Szerző";
     divAuthor.prepend(h2);
 
     let ul = document.querySelector('#idAuthorUnsortedList');
-    addListItemToUnorderedList(ul, "Név: "    + user.name);
-    addListItemToUnorderedList(ul, "Email: "  + user.email);
-    addListItemToUnorderedList(ul, "Tel.: "   + user.phone);
-    addListItemToUnorderedList(ul, "Cégnév: " + user.company.name);
+    addListItemToUnorderedList(ul, "Név",    user.name, false);
+    addListItemToUnorderedList(ul, "Email",  user.email, true);
+    addListItemToUnorderedList(ul, "Tel.",   user.phone, false);
+    addListItemToUnorderedList(ul, "Cégnév", user.company.name, false);
+
+    ul.classList.add("authorInfo");
 }
 
-function addListItemToUnorderedList(ul, contentListItem)
+function addListItemToUnorderedList(ul, tagListItem, contentListItem, isEmail)
 {
     let li = document.createElement('li');
-    li.appendChild(document.createTextNode(contentListItem))
-    ul.appendChild(li);
+
+    if(isEmail)
+    {
+        let link = document.createElement("a");
+        link.setAttribute("href", "mailto:" + contentListItem);
+        link.appendChild(document.createTextNode(tagListItem + ": " + contentListItem));
+        ul.appendChild(link);
+    }
+    else
+    {
+        li.appendChild(document.createTextNode(tagListItem + ": " + contentListItem));
+        ul.appendChild(li);
+    }
 }
 
 getUsers().then(users => addUserInfoToArticle(users[Math.floor(Math.random() * 10)]));
